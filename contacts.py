a566f9b17bf18731d92f4664b4d5876184389949
Rimma Iakhina <lenorium>
2022-06-10 17:08:36 +0300

    добавление контакта

--- contacts.py
+++ contacts.py
@@ -1,32 +1,50 @@
-name = ''
+contact = ''
+delimiter = '-' * 30
 
 
 def add():
-    global name
-    name = input(f'New contact: ')
-    name = name.strip()
+    global contact
+    print('\n----- Create new contact ----- ')
+    contact = enter_contact_info()
+    print(f'\nCongrats! Your new contact is:\n{contact}')
+    print(delimiter)
+
+
+def enter_contact_info():
+    while True:
+        name = input('Name: ').strip()
+        phone_number = input('Phone number: ').strip()
+
+        if name == '' or phone_number == '':
+            print('\nThe name or the phone number is empty. Try again: ')
+        elif not phone_number.isnumeric():
+            print('\nPhone number must contain only digits. Try again: ')
+        else:
+            return name + ' ' + phone_number
 
 
 def search():
+    print('\n----- Search contact ----- ')
+
     keep_searching = True
-
     while keep_searching:
-        search_substring = input(f'\nStart typing a name: ')
+        search_substring = input('Start typing a name or a phone number: ')
         search_substring.lower() \
-                        .strip()
+            .strip()
 
-        prepared_name = name.lower()
-        is_exist = prepared_name.startswith(search_substring)
+        prepared_contact = contact.lower()
+        is_exist = prepared_contact.startswith(search_substring)
 
         if is_exist:
-            print(name)
+            print(contact)
         else:
             print("No results")
 
         keep_searching = input(f"\nWanna find anything else? "
-                               f"Type 'y' for yes or press any key for 'no'") == 'y'
+                               f"Type 'y' for yes or press any key for 'no' ") == 'y'
+
+    print(delimiter)
 
 
 add()
 search()
-
