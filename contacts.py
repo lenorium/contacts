7748310917b319b98f664d4bc994a560c1817bfc
Rimma Iakhina <lenorium>
2022-06-14 11:44:00 +0300

    удаление контакта, выбор действия

--- contacts.py
+++ contacts.py
@@ -1,16 +1,37 @@
 DELIMITER = '\n' + '-' * 30
-contacts = ['abc 94587694576',
-            'znxcvnx 127352',
-            'abcrty 1234567']
+all_contacts = ['abc 94587694576',
+                'znxcvnx 127352',
+                'abcrty 1234567',
+                'test 09586709']
 
 
 def add():
     print('\n----- Create new contact ----- ')
     contact = enter_contact_info()
-    contacts.append(contact)
+    all_contacts.append(contact)
 
-    print(f'\nCongrats! Your new contact is:\n{contact}{DELIMITER}')
+    print(f'\nCongrats! Your new contact is:\n{contact}')
+    print(DELIMITER)
     return contact
+
+
+def delete():
+    print('\n----- Delete contact ----- ')
+
+    if len(all_contacts) == 0:
+        print('Contact list is empty')
+        return
+
+    show_contacts(all_contacts)
+
+    index = int(input('Enter index number of the contact: '))
+    while index < 1 or index > len(all_contacts):
+        index = int(input('The index is incorrect. Try again: '))
+
+    contact = all_contacts[index - 1]
+    all_contacts.remove(contact)
+    print(f"\nYou're right! {contact} doesn't deserve to be on your contact list.")
+    print(DELIMITER)
 
 
 def enter_contact_info():
@@ -26,47 +47,77 @@ def enter_contact_info():
             return name + ' ' + phone_number
 
 
-def search(contact: str):
+def search():
     print('\n------- Search contact ------- ')
 
-    keep_searching = True
-    while keep_searching:
-        search_substring = input('Start typing a name or a phone number: ')\
-            .strip()\
-            .lower()
-        prepared_contact = contact.lower()
-        contact_info = prepared_contact.split(' ')
-        is_present = search_by_name(contact_info[0], search_substring)
-        if not is_present:
-            is_present = search_by_number(contact_info[1], search_substring)
+    search_substring = input('Start typing a name or a phone number: ')
+    result = []
+    for contact in all_contacts:
+        if matches(contact, search_substring):
+            result.append(contact)
 
-        print(contact if is_present else 'No results')
-
-        keep_searching = input(f"\nWanna find anything else? "
-                               f"Type 'y' for yes or press any key for 'no' ") == 'y'
-
+    show_contacts(result) if len(result) != 0 else print('No results')
     print(DELIMITER)
 
 
-def search_by_name(name, substring):
+def matches(contact: str, substring):
+    substring = substring \
+        .strip() \
+        .lower()
+    contact = contact.lower()
+    name, number = contact.split(' ')
+    return matches_by_name(name, substring) or \
+           matches_by_number(number, substring)
+
+
+def matches_by_name(name, substring):
     return name.startswith(substring)
 
 
-def search_by_number(num: str, substring):
+def matches_by_number(num: str, substring):
     result = num.find(substring)
     return result != -1
 
 
-def show_contacts():
+def show_contacts(contacts: list):
     print('\n-------- All contacts -------- ')
-    print(*contacts, sep='\n')
+    for i, contact in enumerate(contacts):
+        print(f'{i + 1}.', contact)
     print(DELIMITER)
 
 
+def ask_action():
+    return input("\nWanna do anything else? "
+                 "Type 'y' for yes or press any key for 'no' ") == 'y'
+
+
 def manage_contacts():
-    contact = add()
-    show_contacts()
-    search(contact)
+    choose_action = True
+    while choose_action:
+        action = input('Choose your action:\n\n'
+                       'Show contacts list (type l)\n'
+                       'Search contact (type s)\n'
+                       'Add new contact (type n)\n'
+                       'Delete contact (type d)\n'
+                       'Exit (type e)\n')
+
+        match action:
+            case 'l':
+                show_contacts(all_contacts)
+                choose_action = ask_action()
+            case 's':
+                search()
+                choose_action = ask_action()
+            case 'n':
+                add()
+                choose_action = ask_action()
+            case 'd':
+                delete()
+                choose_action = ask_action()
+            case 'e':
+                break
+            case _:
+                print('Try again')
 
 
 if __name__ == '__main__':
